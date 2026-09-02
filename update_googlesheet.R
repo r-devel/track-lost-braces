78c80619e4c94aedb076b15f177d4b30b61a7e54
Ella Kaye (Advanced Research Computing) <e.kaye.1@bham.ac.uk>
2026-09-02 14:31:34 +0100

    Fix bugs when updating with CRAN info

--- update_googlesheet.R
+++ update_googlesheet.R
@@ -4,7 +4,10 @@ library(dplyr)
 library(stringr)
 library(cranlogs)
 
+source("track_PRs.R")
+
 # NOTE: lb is shorthand for Lost Braces, a type of documentation NOTE
+# NOTE: vc is shorthand Version Controlled (in GitHub/GitLab/BitBucket)
 
 # Latest CRAN details
 details <- tools::CRAN_check_details(
@@ -15,25 +18,25 @@ Rd_NOTE <- subset(details, Check == "Rd files" & Status == "NOTE")
 pdb <- CRAN_package_db()
 Rd_NOTE <- merge(Rd_NOTE, pdb)
 
-# restrict to packages that use GitHub or equivalent to report issues
-TODO <- Rd_NOTE[
-  grepl(
-    "^http?s://(github.com|gitlab.com|bitbucket.org).*issues",
-    Rd_NOTE$BugReports
-  ),
+# Find packages with "Lost braces" in the Rd NOTE
+Rd_NOTE_lb <- Rd_NOTE[
+  grepl("Lost braces", Rd_NOTE$Output),
 ]
 
-# Find packages with "Lost braces" in the Rd NOTE
-Rd_NOTE_lb <- TODO[grep("Lost braces", TODO$Output), ]
+# restrict to packages that use GitHub or equivalent
+Rd_NOTE_lb_vc <- Rd_NOTE_lb[
+  grepl("github|gitlab|bitbucket|codeberg", Rd_NOTE_lb$BugReports) |
+    grepl("github|gitlab|bitbucket|codeberg", Rd_NOTE_lb$URL),
+]
 
 # CRAN info for GoogleSheet
-from_CRAN <- Rd_NOTE_lb |>
+from_CRAN <- Rd_NOTE_lb_vc |>
   select(Package, Version, Maintainer, Output, URL, BugReports)
 
 # read in sheet
 current_sheet <- read_sheet(
   "https://docs.google.com/spreadsheets/d/1qL5s2okfQmh_ufwh3MS6rJPzIlLmJzIN2g9u2loFzkA/edit?gid=1451772479#gid=1451772479",
-  col_types = "cclcccccccii"
+  col_types = "cclcTccccccii"
 )
 
 # Find packages which still have this NOTE *or* have a PR_status
@@ -52,7 +55,7 @@ pdb_version <- pdb |>
   filter(Package %in% all_rows$Package) |>
   select(Package, Version)
 
-Rd_NOTE_lb_output <- Rd_NOTE_lb |>
+Rd_NOTE_lb_vc_output <- Rd_NOTE_lb_vc |>
   filter(Package %in% all_rows$Package) |>
   select(Package, Output)
 
@@ -63,9 +66,10 @@ downloads <- cran_downloads(all_rows$Package, "last-month") |>
 # Update rows for sheet
 updated_rows <- all_rows |>
   mutate(has_lb_NOTE = Package %in% from_CRAN$Package) |>
+  mutate(Version = if_else(Package %in% pdb$Package, Version, NA)) |>
   rows_update(pdb_version, by = "Package") |>
   mutate(Output = if_else(has_lb_NOTE, Output, NA)) |>
-  rows_update(Rd_NOTE_lb_output, by = "Package") |>
+  rows_update(Rd_NOTE_lb_vc_output, by = "Package") |>
   rows_update(downloads, by = "Package")
 
 # TODO: update programmatically:
@@ -74,9 +78,21 @@ updated_rows <- all_rows |>
 
 # TODO: write back to GoogleSheets
 
-# Next packages to submit PRs to:
+# Next packages to submit manual PRs to:
 updated_rows |>
   arrange(desc(downloads_last_month)) |>
   filter(is.na(PR_status)) |>
   head() |>
   select(Package, BugReports)
+
+# For CRAN team: packages that need to make a release or merge a PR
+updated_rows |>
+  filter(has_lb_NOTE & !is.na(PR_status))
+
+# For R Contributors: keep track of successes!
+updated_rows |>
+  filter(!has_lb_NOTE & !is.na(PR_status))
+
+updated_rows |>
+  filter(Package == "survMisc") |>
+  View()
