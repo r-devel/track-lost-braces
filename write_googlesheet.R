de3ca3c9b72961899973122ec619e72136694538
Ella Kaye (Advanced Research Computing) <e.kaye.1@bham.ac.uk>
2026-09-03 15:33:26 +0100

    Remove data validation (doesn't preserve colours)

--- write_googlesheet.R
+++ write_googlesheet.R
@@ -33,39 +33,3 @@ updated_sheet <- track_lost_braces |>
 
 # ss defined in read_googlesheet.R
 write_sheet(updated_sheet, ss, sheet = "Latest")
-
-# Apply the data validation rule to the PR_status column
-
-# Get the numeric sheetId for the "Latest" tab
-props <- sheet_properties(ss)
-sheet_id_latest <- props$id[props$name == "Latest"]
-
-# Build and send the setDataValidation request
-req <- list(
-  setDataValidation = list(
-    range = list(
-      sheetId = sheet_id_latest,
-      startRowIndex = 1, # 0-indexed -> row 2, i.e. skips the header
-      endRowIndex = 400, # last row with data
-      startColumnIndex = 1, # 0-indexed -> column B, PR_status
-      endColumnIndex = 2
-    ),
-    rule = list(
-      condition = list(
-        type = "ONE_OF_RANGE",
-        values = list(
-          list(userEnteredValue = "='Data Validation'!A2:A10")
-        )
-      ),
-      showCustomUi = TRUE,
-      strict = TRUE
-    )
-  )
-)
-
-bu_req <- request_generate(
-  "sheets.spreadsheets.batchUpdate",
-  params = list(spreadsheetId = ss, requests = list(req))
-)
-resp_raw <- request_make(bu_req)
-gargle::response_process(resp_raw)
