2acf0ed800d2cc414ff2821489a024c8816789a1
Ella Kaye (Advanced Research Computing) <e.kaye.1@bham.ac.uk>
2026-09-03 15:32:50 +0100

    Add data validation programmatically

--- read_googlesheet.R
+++ read_googlesheet.R
@@ -1,9 +1,11 @@
 library(googlesheets4)
 
 # read in sheet
-sheet_url <- "https://docs.google.com/spreadsheets/d/1qL5s2okfQmh_ufwh3MS6rJPzIlLmJzIN2g9u2loFzkA/edit?gid=1451772479#gid=1451772479"
+
+ss <- "1qL5s2okfQmh_ufwh3MS6rJPzIlLmJzIN2g9u2loFzkA"
+
 current_sheet <- read_sheet(
-  sheet_url,
+  ss,
   sheet = "Latest",
   col_types = "cclcTccccccii"
 )
--- write_googlesheet.R
+++ write_googlesheet.R
@@ -1,9 +1,8 @@
-# NOTES
 # Script takes ~1 minute to run on an M5 MackBook Pro
 # Script requires access to the rowforwards Google account for authentication
 # Any "PROBLEMS" showing in Positron resolve after source("track_lost_braces.R") is run
 # Tracking spreadsheet is
-# https://docs.google.com/spreadsheets/d/1qL5s2okfQmh_ufwh3MS6rJPzIlLmJzIN2g9u2loFzkA/edit?gid=500184850#gid=500184850
+# https://docs.google.com/spreadsheets/d/1qL5s2okfQmh_ufwh3MS6rJPzIlLmJzIN2g9u2loFzkA
 
 source("track_lost_braces.R")
 
@@ -32,5 +31,41 @@ updated_sheet <- track_lost_braces |>
     ))
   )
 
-# sheet_url defined in read_googlesheet.R
-write_sheet(updated_sheet, ss = sheet_url, sheet = "Latest")
+# ss defined in read_googlesheet.R
+write_sheet(updated_sheet, ss, sheet = "Latest")
+
+# Apply the data validation rule to the PR_status column
+
+# Get the numeric sheetId for the "Latest" tab
+props <- sheet_properties(ss)
+sheet_id_latest <- props$id[props$name == "Latest"]
+
+# Build and send the setDataValidation request
+req <- list(
+  setDataValidation = list(
+    range = list(
+      sheetId = sheet_id_latest,
+      startRowIndex = 1, # 0-indexed -> row 2, i.e. skips the header
+      endRowIndex = 400, # last row with data
+      startColumnIndex = 1, # 0-indexed -> column B, PR_status
+      endColumnIndex = 2
+    ),
+    rule = list(
+      condition = list(
+        type = "ONE_OF_RANGE",
+        values = list(
+          list(userEnteredValue = "='Data Validation'!A2:A10")
+        )
+      ),
+      showCustomUi = TRUE,
+      strict = TRUE
+    )
+  )
+)
+
+bu_req <- request_generate(
+  "sheets.spreadsheets.batchUpdate",
+  params = list(spreadsheetId = ss, requests = list(req))
+)
+resp_raw <- request_make(bu_req)
+gargle::response_process(resp_raw)
