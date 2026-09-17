eb84afc0742d480826f4c338376f29af70127b5a
Ella Kaye (Advanced Research Computing) <e.kaye.1@bham.ac.uk>
2026-09-17 18:34:06 +0100

    Add auth for GHA

--- read_googlesheet.R
+++ read_googlesheet.R
@@ -1,7 +1,13 @@
 library(googlesheets4)
 
-# read in sheet
+# Programmatic auth for unattended runs (e.g. GitHub Actions)
+if (Sys.getenv("GOOGLE_SERVICE_ACCOUNT_PATH") != "") {
+  gs4_auth(path = Sys.getenv("GOOGLE_SERVICE_ACCOUNT_PATH"))
+} else {
+  gs4_auth() # local interactive flow, cached as before
+}
 
+# read in sheet
 ss <- "1qL5s2okfQmh_ufwh3MS6rJPzIlLmJzIN2g9u2loFzkA"
 
 current_sheet <- read_sheet(
