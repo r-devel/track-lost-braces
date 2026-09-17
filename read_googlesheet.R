a1085c38ffcd3d950c4040a0bda7fe2223714d31
Ella Kaye (Advanced Research Computing) <e.kaye.1@bham.ac.uk>
2026-09-17 18:51:25 +0100

    Programmatically rename Latest with date

--- read_googlesheet.R
+++ read_googlesheet.R
@@ -10,8 +10,9 @@ if (Sys.getenv("GOOGLE_SERVICE_ACCOUNT_PATH") != "") {
 # read in sheet
 ss <- "1qL5s2okfQmh_ufwh3MS6rJPzIlLmJzIN2g9u2loFzkA"
 
+
 current_sheet <- read_sheet(
   ss,
-  sheet = "Latest",
+  sheet = 1, # "Latest (YYYY-MM-DD)", where date is the last updated.
   col_types = "cclcTccccccii"
 )
--- write_googlesheet.R
+++ write_googlesheet.R
@@ -31,5 +31,11 @@ updated_sheet <- track_lost_braces |>
     ))
   )
 
+
 # ss defined in read_googlesheet.R
-write_sheet(updated_sheet, ss, sheet = "Latest")
+# first sheet is "Latest (YYYY-MM-DD)" which is when previously updated
+new_name <- paste0("Latest (", Sys.Date(), ")")
+sheet_rename(ss, sheet = 1, new_name)
+
+# defaults renaming to the first visible sheet
+write_sheet(updated_sheet, ss, new_name)
